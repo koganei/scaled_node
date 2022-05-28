6b678418907d4eb1b836f3777d9847b865ded988
Marc Khoury <khoury.marc@gmail.com>
2022-05-28 14:49:50 -0400

    Update

--- core/index.js
+++ core/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 app.get('/', function(req, res) {
-	res.send('Hello Experience');
+	res.send('Hello Core');
 });
 
 app.listen('3000', function() {
